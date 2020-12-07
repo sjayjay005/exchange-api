c56585f5dddcf43a5b19a663fc23ffa369d48e5f
sjayjay005 <sjayjay005@gmail.com>
2020-12-07 20:47:14 +0200

    Adding Request, Response and Router with htaccess

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
-
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Lib\App;
+use App\Lib\Router;
+use App\Lib\Request;
+use App\Lib\Response;
+
+Router::get('/', function () {
+    echo 'Hello API Exchange';
+});
+
+Router::get('/currency/([0-9]*)', function (Request $req, Response $res) {
+    $res->toJSON([
+        'currency' =>  ['id' => $req->params[0]],
+        'status' => 'ok'
+    ]);
+});
 
 App::run();
